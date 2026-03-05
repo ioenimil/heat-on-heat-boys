1c5f6f386a6939e04ed194badf086f0e45d432c6
RoalndBissah10 <rolandbissah10@gmail.com>
2026-03-05 07:47:08 +0000

    feature: Created a test method to verify that the api/health endpoint returns 200 status code

---
+++ qa/api-tests/src/test/java/com/amalitech/qa/api_health_status/HealthApiTest.java
@@ -0,0 +1,32 @@
+package com.amalitech.qa.api_health_status;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class HealthApiTest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
+//    Testing to confirm if the health endpoint returns 200 status code
+    @Test
+    public void testHealthEndpointReturnsHttp200() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/health")
+                .then()
+                .statusCode(200);
+    }
+
+
+
+
+}
