ebb25ab48aad328fb5a3daf28e0e76b26ad2c289
Isaac Obo Enimil <isaac.enimil@amalitech.com>
2026-03-06 10:37:15 +0000

    Adds Terraform provider and global variables config

---
+++ ops-engine-room/infrastructure/stagging-environment/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region to deploy to"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
