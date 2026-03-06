3264d596c91b1d17cbef56895f36b6307e35afb0
Isaac Obo Enimil <isaac.enimil@amalitech.com>
2026-03-06 10:37:16 +0000

    Adds MWAA environment and IAM integration

---
+++ ops-engine-room/infrastructure/stagging-environment/modules/mwaa/outputs.tf
@@ -0,0 +1,1 @@
+output "mwaa_environment_name" { value = aws_mwaa_environment.main.name }
---
+++ ops-engine-room/infrastructure/stagging-environment/modules/mwaa/variables.tf
@@ -0,0 +1,3 @@
+variable "mwaa_sg_id" { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "mwaa_bucket_arn" { type = string }
